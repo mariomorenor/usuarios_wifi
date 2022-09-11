51c05c338607d60b7c17a98695b4b87c1d0f8d04
Mario Moreno <mario_morenor@hotmail.com>
2022-09-11 15:42:01 -0500

    Agregado funcionalidad de agregar usuario

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,13 +21,36 @@ router.post("/getUser", async (req, res) => {
 });
 
 router.post("/updatePassword", async (req, res) => {
-  console.log(req.body);
-
   let [result, metadata] = await sequelize.query(
     `UPDATE radcheck SET value = '${req.body.user.value}' WHERE id = ${req.body.user.id}`
   );
-  
+
   res.send(metadata);
+});
+
+router.post("/createUser", async (req, res) => {
+  let user = req.body;
+  try {
+    let [result, metadata] = await sequelize.query(
+      "INSERT INTO radcheck(id,identity,username,attribute,op,value,name,last_name,email,`career-unit`,`period-position`,date,`expire`,`status`) VALUES (0,:identity,:username,'Cleartext-Password',':=',:value,:name,:last_name,:email,:career,NULL,NOW(),NULL,1)",
+      {
+        replacements: {
+          identity: user.identity,
+          username: user.username,
+          value: user.password,
+          name: user.name,
+          last_name: user.last_name,
+          email: user.email,
+          career: user.career,
+        },
+      }
+    );
+
+    res.send({ id: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error,)
+  }
 });
 
 module.exports = router;
