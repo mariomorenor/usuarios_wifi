acc5ab801fa165bd6e6e76d475e15a1afc0161b1
Mario Moreno <mario_morenor@hotmail.com>
2022-09-11 13:05:18 -0500

    cambios

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,6 @@ app.use(express.json())
 app.use(cors());
 app.use(require("./routes/routes"));
 
-app.listen(port,'0.0.0.0', () => {
+app.listen(port, () => {
   console.log("Server Escuchando en puerto", port);
 });
