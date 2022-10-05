ffb865ae3a29ddd4c7abd6e11b4efac7385d6a51
Mario Moreno <mario_morenor@hotmail.com>
2022-10-05 08:23:08 -0500

    Fix Se cambia el modo de conexion de sequelize para solventar el problema de "Database Version"

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,14 @@
 import { Sequelize, DataTypes } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "mariadb",
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  logging:false
-});
-
-
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "radius",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "",
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+    logging: false,
+  }
+);
 
 export { sequelize };
